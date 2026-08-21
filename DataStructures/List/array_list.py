7cb63488c720e6e12c64abe6ab2e000fed16d160
Juan Peñaloza <74742342+Tronxward16@users.noreply.github.com>
2026-08-21 00:56:21 +0000

    Implementacion funciones basicas

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -18,3 +18,25 @@ def is_present(my_list, element, cmp_function):
             return position
 
     return -1
+
+
+def add_first(my_list, element):
+    my_list["elements"].insert(0, element)
+    my_list["size"] += 1
+    return my_list
+
+
+def add_last(my_list, element):
+    my_list["elements"].append(element)
+    my_list["size"] += 1
+    return my_list
+
+
+def size(my_list):
+    return my_list["size"]
+
+
+def first_element(my_list):
+    if my_list["size"] == 0:
+        raise IndexError("list index out of range")
+    return my_list["elements"][0]
--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -26,3 +26,38 @@ def is_present(my_list, element, cmp_function):
     if not is_in_array:
         count = -1
     return count
+
+
+def add_first(my_list, element):
+    node = {"info": element, "next": my_list["first"]}
+    my_list["first"] = node
+
+    if my_list["size"] == 0:
+        my_list["last"] = node
+
+    my_list["size"] += 1
+    return my_list
+
+
+def add_last(my_list, element):
+    node = {"info": element, "next": None}
+
+    if my_list["size"] == 0:
+        my_list["first"] = node
+        my_list["last"] = node
+    else:
+        my_list["last"]["next"] = node
+        my_list["last"] = node
+
+    my_list["size"] += 1
+    return my_list
+
+
+def size(my_list):
+    return my_list["size"]
+
+
+def first_element(my_list):
+    if my_list["size"] == 0:
+        raise IndexError("list index out of range")
+    return my_list["first"]["info"]
