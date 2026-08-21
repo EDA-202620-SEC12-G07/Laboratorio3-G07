2175c0f8d394e93bb1a97c9bb7aebddb9e7c929c
Andresjvm <andresjosevm@gmail.com>
2026-08-20 19:46:30 -0500

    modificaciones sll

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -4,3 +4,25 @@ def new_list():
                "size": 0,
     } 
     return newlist
+
+def get_element(my_list, pos):
+    searchpos = 0
+    node = my_list["first"]
+    while searchpos < pos:
+        node = node["next"]
+        searchpos += 1
+    return node["info"]
+
+def is_present(my_list, element, cmp_function):
+    is_in_array = False
+    temp = my_list["first"]
+    count = 0
+    while not is_in_array and temp is not None:
+        if cmp_function(element, temp["info"]) == 0:
+            is_in_array = True
+        else:
+            temp = temp["next"]
+            count += 1
+    if not is_in_array:
+        count = -1
+    return count
