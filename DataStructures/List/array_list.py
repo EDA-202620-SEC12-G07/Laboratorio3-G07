878a8e483092bc7374932c1935e47987a8931439
Juan Peñaloza <74742342+Tronxward16@users.noreply.github.com>
2026-08-21 00:43:23 +0000

    Modificaciones en array_list

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -1,6 +1,20 @@
 def new_list():
-    new_list = {
-        'elements': [],
-        'size': 0,
+    newlist = {
+        "elements": [],
+        "size": 0
     }
-    return new_list
+    return newlist
+
+
+def get_element(my_list, index):
+    return my_list["elements"][index]
+
+
+def is_present(my_list, element, cmp_function):
+    for position in range(my_list["size"]):
+        info = my_list["elements"][position]
+
+        if cmp_function(element, info) == 0:
+            return position
+
+    return -1
